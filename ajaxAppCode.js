c91e048b7cc8eb4efc314843a5a3c69ccb5c1bbf
Elizabeth Levi <elizabeth.wilson2010@yahoo.com>
2024-04-13 11:54:56 -0400

    Add files via upload

---
+++ ajaxAppCode.js
@@ -0,0 +1,25 @@
+async function getData(selected_major){
+
+	var response = await fetch('cit5students.json');
+
+	if(response.ok){
+		var data = await response.json();
+		
+		//Filter data for user input major
+		major_items = data.filter((item) => item.major == selected_major);
+		
+		var tempTxt = document.getElementById('majorTemplate').innerHTML;
+		var compileTempTxt = Handlebars.compile(tempTxt);
+		
+		compiledHtml = compiledTempTxt({rows: major_items})
+		document.getElementById('majorTable').innerHTML = compiledHtml;
+	}
+	
+	else{
+	
+		document.querySelector('#majorTable').innerHTML = "There was an error, or major items not found";
+		
+		}
+
+
+}
